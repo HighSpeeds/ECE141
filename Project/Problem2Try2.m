7871070bca441216d0f38d551660b91658a815f9
HighSpeeds <lawrencerliu@ucla.edu>
2022-06-02 23:46:20 -0700

    Correct Problem 2

---
+++ Project/Problem2Try2.m
@@ -0,0 +1,7 @@
+v=15.6464;
+l_r=1.7;
+k_p=4/l_r;
+
+sys = tf([k_p*v*l_r k_p*v^2],[l_r  k_p*v*l_r k_p*v^2]);
+h = pzplot(sys);
+grid on
--- Project/Problem3Try1.m
+++ Project/Problem3Try1.m
@@ -1,18 +1,22 @@
-psi_sweep=0:0.25:2*pi;
-y_sweep=-3:0.1:-1;
+l_r=1.7;
+
+v=15.6464;
+
+k_p=4/l_r;
+
+psi_sweep=-pi/2:0.1:pi/2;
+y_sweep=-2:0.05:-1;
 t_final=40;
 valid = zeros(length(psi_sweep),length(y_sweep));
 for i=1:length(psi_sweep)
     psi_initial=psi_sweep(i);
-    i
     psi_initial
     for j=1:length(y_sweep)
         y_initial=10^(y_sweep(j));
         a=sim("Problem3.slx");
 
         data=a.yout.getElement("y");
-        
-        if max(abs(data.Values.Data))<0.6
+        if max(abs(data.Values.Data))<=y_initial && min(data.Values.Data)>=0
             valid(i,j)=1;
         end
     end
